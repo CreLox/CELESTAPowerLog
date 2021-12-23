9c629b0c095a2595b631fee16167de7caa301478
Chu Chen <chenchu@umich.edu>
2021-12-23 02:07:13 -0500

    Add files via upload

---
+++ reconstitutePulse.m
@@ -0,0 +1,27 @@
+% FigHandle = reconstitutePulse(CelestaLogPath, ExistingFigHandle(OPTIONAL))
+function FigHandle = reconstitutePulse(CelestaLogPath, ExistingFigHandle)
+    % Parameter settings
+    FontSize = 15;
+    LineWidth = 1.5;
+    BoxOnOrOff = 'off';
+    
+    % Read the log file (comma-separated)
+    LogTable = readtable(CelestaLogPath);
+    InstantaneousPowers = LogTable.Var2;
+    
+    % Calculate the empirical cumulative distribution (plotting inhibited)
+    [ECP, EvalPts] = ecdf(InstantaneousPowers);
+    
+    % Plot
+    if ~exist('ExistingFigHandle', 'var') || isempty(ExistingFigHandle)
+        FigHandle = figure;
+        stairs(ECP, EvalPts, 'k', 'LineWidth', LineWidth);
+    else
+        FigHandle = figure(ExistingFigHandle);
+        hold on;
+        stairs(ECP, EvalPts, 'LineWidth', LineWidth);
+    end
+    xlabel('Empirical cumulative probability'); % ECP
+    ylabel('Instantaneous power (mW)');
+    set(gca, 'FontSize', FontSize, 'LineWidth', LineWidth, 'box', BoxOnOrOff);
+end
