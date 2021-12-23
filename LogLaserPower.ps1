173344899609afc22ecc08d35e35b44557acc31b
Chu Chen <chenchu@umich.edu>
2021-12-23 15:54:28 -0500

    Update LogLaserPower.ps1

--- LogLaserPower.ps1
+++ LogLaserPower.ps1
@@ -1,4 +1,5 @@
 $Interval = 12
+$Threshold = 5
 
 $URL = "http://192.168.201.200/service/?command=GET%20CHPWRWATTS%20"+$args[0]
 $Date = Get-Date -Format "yyyyMMdd"
@@ -9,7 +10,7 @@ while (0 -eq 0) {
 	$TimeStamp = Get-Date -Format "HH:mm:ss.fff"
 	
 	$HTML.Content -match '"message": "(?<Status>.+) CHPWRWATTS (?<mWPower>.+)"' >$null
-	if (($Matches.Status -eq "A") -and ($Matches.mWPower -ne "0.0")) {
+	if (($Matches.Status -eq "A") -and ($Matches.mWPower -ge $Threshold)) {
         	$NewLine = $TimeStamp+","+$Matches.mWPower
 		Add-Content $File $NewLine
 	}
